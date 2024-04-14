97bf629f654106f152d61eba1453e0f89f22a9c8
Migue0301 <miguel.paredesperez1@gmail.com>
2024-04-14 16:22:39 -0600

    last changes

--- seani/seani/urls.py
+++ seani/seani/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 from exam import views
+from seani.seani import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,6 @@ urlpatterns = [
     path('exam/', include('exam.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
